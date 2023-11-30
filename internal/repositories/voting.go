c1f43af1181b14da1189de75ae6a7595c00c4a43
Kirill <kir4ick@mail.ru>
2023-12-01 00:26:40 +0400

    сделал запросы в бд для подсчета голосов и процента определенного типа голоса

--- internal/repositories/voting.go
+++ internal/repositories/voting.go
@@ -30,3 +30,92 @@ func (repository *Repository) InsertNewVoting(ctx *context.Context, vote *entity
 	_, err := repository.database.Collection("vote").InsertOne(*ctx, vote)
 	return vote.VotingId, err
 }
+
+func (repository *Repository) GetVotingCountVotes(ctx *context.Context, votingId string) ([]entity.Vote, error) {
+
+	function := bson.A{
+		bson.D{{"$match", bson.D{{"votingid", votingId}}}},
+		bson.D{{"$unwind", "$votes"}},
+		bson.D{
+			{"$group",
+				bson.D{
+					{"_id", "$votes.optionid"},
+					{"count", bson.D{{"$count", bson.D{}}}},
+				},
+			},
+		},
+	}
+
+	result, err := repository.database.Collection("vote").Aggregate(*ctx, function)
+	if err != nil {
+		return nil, err
+	}
+
+	var resultArray []entity.Vote
+	err = result.All(*ctx, &resultArray)
+
+	return resultArray, err
+}
+
+func (repository *Repository) GetVotesCondition(ctx *context.Context, votingId string, optionId string) {
+
+	function := bson.A{
+		bson.D{{"$match", bson.D{{"votingid", votingId}}}},
+		bson.D{
+			{"$project",
+				bson.D{
+					{"_id", 0},
+					{"votesCount", bson.D{{"$size", "$votes"}}},
+					{"votes", "$votes"},
+					{"votingid", "$votingid"},
+				},
+			},
+		},
+		bson.D{{"$unwind", "$votes"}},
+		bson.D{
+			{"$group",
+				bson.D{
+					{"_id", "$votes.optionid"},
+					{"count", bson.D{{"$count", bson.D{}}}},
+					{"votesCount", bson.D{{"$push", "$votesCount"}}},
+				},
+			},
+		},
+		bson.D{
+			{"$project",
+				bson.D{
+					{"_id", "$_id"},
+					{"count", "$count"},
+					{"votesCount", bson.D{{"$first", "$votesCount"}}},
+				},
+			},
+		},
+		bson.D{
+			{"$project",
+				bson.D{
+					{"_id", "$_id"},
+					{"percents",
+						bson.D{
+							{"$multiply",
+								bson.A{
+									bson.D{
+										{"$divide",
+											bson.A{
+												"$count",
+												"$votesCount",
+											},
+										},
+									},
+									100,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		bson.D{{"$match", bson.D{{"_id", optionId}}}},
+	}
+
+	result, err := repository.database.Collection("vote").Aggregate(*ctx, function)
+}
